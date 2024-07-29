d84791e15d11c19052b27ff090690b6217aa77ae
Web Master <webmaster@dc1rec.local>
2024-07-29 08:35:33 -0600

    debug veracruz v2

--- app/Http/Controllers/ExtrasUnimexController.php
+++ app/Http/Controllers/ExtrasUnimexController.php
@@ -195,8 +195,8 @@ class ExtrasUnimexController extends Controller
     public function setVariableCarreraCombo($idCarrera, $nombreCarrera)
     {
 
-        echo $idCarrera;
-        echo $nombreCarrera;
+        //echo $idCarrera;
+        //echo $nombreCarrera;
 
         //! si el id de carrera viene nulo o vacio en su cadena quiere decir que la opcion de selecciona tu carrera es la que esta en el select
         if ($idCarrera == null || $idCarrera == "") {
@@ -226,6 +226,9 @@ class ExtrasUnimexController extends Controller
             $respuesta['id'] = session("idCarreraComboResguardo");
             $respuesta['mensaje'] = "variable establecida";
             //echo('hay algo en el select');
+
+            echo session("nombreCarreraComboResguardo");
+            echo session("idCarreraComboResguardo");
         }
 
         return response()->json($respuesta);
