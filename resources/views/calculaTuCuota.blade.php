6b7daa170cbd7cf7b8c9f666b75cb745dec78710
Web Master <webmaster@dc1rec.local>
2024-10-10 09:54:39 -0600

    actualizacion del carrucel de calculadora de cuotas se agrego intervalos

--- resources/views/calculaTuCuota.blade.php
+++ resources/views/calculaTuCuota.blade.php
@@ -100,35 +100,35 @@
                 </form>
             </div>
             <div class="col-12 col-md-9 col-lg-9 mt-2 mt-md-0 p-0 px-md-3">
-                <div id="carouselExampleIndicators" class="carousel slide">
-                    <div class="carousel-indicators">
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0"
-                            class="active" aria-current="true" aria-label="Slide 1"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
-                            aria-label="Slide 2"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
-                            aria-label="Slide 3"></button>
-                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
-                            aria-label="Slide 3"></button>
-                    </div>
+                <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
-                        <div class="carousel-item active">
+                        <div class="carousel-item active" data-bs-interval="20000">
                             <img src="{{ asset('assets/img/calculadora_de_cuotas/2024/2024_1.jpg') }}"
                                 class="d-block w-100" alt="...">
                         </div>
-                        <div class="carousel-item">
+                        <div class="carousel-item" data-bs-interval="20000">
                             <img src="{{ asset('assets/img/calculadora_de_cuotas/2.jpg') }}" class="d-block w-100"
                                 alt="...">
                         </div>
-                        <div class="carousel-item">
+                        <div class="carousel-item" data-bs-interval="20000">
                             <img src="{{ asset('assets/img/calculadora_de_cuotas/3.jpg') }}" class="d-block w-100"
                                 alt="...">
                         </div>
-                        <div class="carousel-item">
+                        <div class="carousel-item" data-bs-interval="20000">
                             <img src="{{ asset('assets/img/calculadora_de_cuotas/4.jpg') }}" class="d-block w-100"
                                 alt="...">
                         </div>
                     </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleInterval"
+                        data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleInterval"
+                        data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
                 <div id="informacionCRM" class="row mt-3 d-none">
                     <h2 class="text-center mb-3" style="color: #004b93">¡Estás a un paso de ser UNIMEXITARIO!</h2>
